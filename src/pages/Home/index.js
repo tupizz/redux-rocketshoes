ddc1f47e8229c9c98e8f0418e01950f58f0db2d5
tupizz <tadeu.tupiz@gmail.com>
2019-08-22 00:12:07 -0300

    :building_construction: Building Home Page (List of products)

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-    return <div />;
+    return (
+        <ProductList>
+            {[1, 2, 3, 4, 5, 6].map(() => (
+                <li>
+                    <img
+                        src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_detalhe2.jpg?resize=326:*"
+                        alt="Tênis de caminhada leve e confortável"
+                    />
+                    <strong>Tênis muito bacana</strong>
+                    <span>R$129,90</span>
+
+                    <button type="button">
+                        <div>
+                            <MdAddShoppingCart size={16} color="#FFF" /> 3
+                        </div>
+
+                        <span>ADICIONAR AO CARRINHO</span>
+                    </button>
+                </li>
+            ))}
+        </ProductList>
+    );
 }
