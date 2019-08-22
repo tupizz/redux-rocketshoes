4184e974eccc173f95dde690502ef6eca969a945
tupizz <tadeu.tupiz@gmail.com>
2019-08-21 23:45:02 -0300

    :sparkles: GlobalStyles, Router and Header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import GlobalStyle from './styles/global';
+import Header from './components/Header';
+import Routes from './routes';
 
 function App() {
-    return <div className="App">Hello World</div>;
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes />
+            <GlobalStyle />
+        </BrowserRouter>
+    );
 }
 
 export default App;
