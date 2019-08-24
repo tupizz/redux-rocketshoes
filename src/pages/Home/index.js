7b6351496b0899f82e5034abb8eb8938d19d98d2
tupizz <tadeu.tupiz@gmail.com>
2019-08-23 22:26:49 -0300

    :alembic: Getting products from api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,29 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+import api from '../../services/api';
+import { formatPrice } from '../../utils/format';
 
 import { ProductList } from './styles';
 
-export default function Home() {
-    return (
-        <ProductList>
-            {[1, 2, 3, 4, 5, 6].map(() => (
-                <li>
-                    <img
-                        src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_detalhe2.jpg?resize=326:*"
-                        alt="Tênis de caminhada leve e confortável"
-                    />
-                    <strong>Tênis muito bacana</strong>
-                    <span>R$129,90</span>
+export default class Home extends Component {
+    state = {
+        products: [],
+    };
 
-                    <button type="button">
-                        <div>
-                            <MdAddShoppingCart size={16} color="#FFF" /> 3
-                        </div>
+    async componentDidMount() {
+        const response = await api.get('products');
 
-                        <span>ADICIONAR AO CARRINHO</span>
-                    </button>
-                </li>
-            ))}
-        </ProductList>
-    );
+        const data = response.data.map(product => ({
+            ...product,
+            priceFormatted: formatPrice(product.price),
+        }));
+
+        this.setState({
+            products: data,
+        });
+    }
+
+    render() {
+        const { products } = this.state;
+        return (
+            <ProductList>
+                {products.map(product => (
+                    <li key={product.id}>
+                        <img src={product.image} alt={product.title} />
+                        <strong>{product.title}</strong>
+                        <span>{product.priceFormatted}</span>
+
+                        <button type="button">
+                            <div>
+                                <MdAddShoppingCart size={16} color="#FFF" /> 3
+                            </div>
+
+                            <span>ADICIONAR AO CARRINHO</span>
+                        </button>
+                    </li>
+                ))}
+            </ProductList>
+        );
+    }
 }
